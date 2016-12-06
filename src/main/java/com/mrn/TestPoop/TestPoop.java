e58ff8bc45bbc2b761e4bc9035837cb774d9aab2
Michael-Nunley <Michael.ray.nunley@gmail.com>
2016-12-06 04:50:35 -0800

    mcmod

--- src/main/java/com/mrn/TestPoop/TestPoop.java
+++ src/main/java/com/mrn/TestPoop/TestPoop.java
@@ -1,9 +1,33 @@
 package com.mrn.TestPoop;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid="TestPoop", name="TestPoop", version = "1.7.10-1.0")
 public class TestPoop
 {
-    
+
+    @Mod.Instance("TestPoop")
+    public static TestPoop instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
 }
